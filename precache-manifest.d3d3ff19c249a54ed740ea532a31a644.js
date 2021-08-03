71069835002ee70c86034b8ded4ad2683cd8c4ff
Сакевич Анастасія <asakevych@ASMAC.local>
2021-08-04 00:12:45 +0300

    Updates

---
+++ precache-manifest.d3d3ff19c249a54ed740ea532a31a644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c94faab857d39f21c8a7c01685e8ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "965d47e073d551d08910",
+    "url": "./static/js/2.98e7adc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.98e7adc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a0d06f0f67db337665",
+    "url": "./static/js/main.a74430dc.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
